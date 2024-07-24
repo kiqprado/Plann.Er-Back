b730a25c43a392addc403163edcabeefa53f402c
Kaique Prado <kiqprado@outlook.com>
2024-07-24 09:47:52 -0300

    feat: fixing how to show activities routes on Get method

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
 import { confirmParticipants } from './routes/confirm-participant'
 import { createActivity } from './routes/create-activity'
+import { getActivities } from './routes/get-activities'
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
 
 
@@ -20,6 +21,7 @@ app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
 app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3333}).then(() => {
   console.log('Ain ze da manga')
