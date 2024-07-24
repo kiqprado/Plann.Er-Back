a2539fb0d4856979557030c221a7dbb94affef79
Kaique Prado <kiqprado@outlook.com>
2024-07-24 08:06:44 -0300

    feat: Create Participants confirmed verification.

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { dayjs } from '../lib/dayjs'
+import { getMailClient } from '../lib/mail'
+import nodemailer from 'nodemailer'
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -9,7 +13,75 @@ export async function confirmTrip(app: FastifyInstance) {
         tripId: z.string().uuid(),
       })
     },
-  }, async (request) => {
-    return { tripId: request.params.tripId }
+  }, async (request, reply ) => {
+    const { tripId } = request.params
+
+    const trip = await prisma.trip.findUnique({
+      where: {
+        id: tripId
+      },
+      include: {
+        participants: {
+          where: {
+            is_owner: false
+          }
+        }
+      }
+    })
+
+    if(!trip) {
+      throw new Error('Trip not found')
+    }
+
+    if(trip.is_confirmed) {
+      return reply.redirect(`http://localhost:5173/trips/${tripId}`)
+    }
+
+    await prisma.trip.update({
+      where: { id: tripId},
+      data: { is_confirmed: true },
+    })
+
+    const formattedStartDate = dayjs(trip.starts_at).format('LL')
+    const formattedEndDate = dayjs(trip.ends_at).format('LL')
+
+    const confirmationLink =  `http://localhost:3333/participants/${participant.id}/confirm`
+
+    const mail = await getMailClient()
+
+    await Promise.all(
+      trip.participants.map( async (participant) => {
+        const message = await mail.sendMail({
+          from: {
+            name: 'Equipe Plann.Er',
+            address: 'hey@plann.er'
+          },
+    
+          to:  participant.email,
+          subject: `Confirme a sua presença na viagem para ${trip.destination} em ${formattedStartDate}.`,
+          html: `
+            <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+              <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+              <p></p>
+              <p>Para confirmar sua presença, clique no link abaixo:</p>
+              <p></p>
+              <a href="${confirmationLink}">Confirmar presença</a>
+              <p></p>
+              <p>Caso esteja usando o dispositivo móvel, você também pode confirmar a criação da viagem pelos aplicativos:</p>
+              <p></p>
+              <a>Aplicativo para Iphone</a>
+              <a>Aplicativo para Android</a>
+              <p></p>
+              <p>Caso você não saiba do que  se trata esse e-mail, ou não poderá estar presente, apenas ignore este e-mail.</p>
+            </div>
+          `.trim()
+        })
+    
+        console.log(nodemailer.getTestMessageUrl(message))
+    
+      })
+    )
+
+    return reply.redirect(`http://localhosto:5173/trips/${tripId}`)
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipants } from './routes/confirm-participant'
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
 
 const app = fastify()
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipants)
 
 app.listen({ port: 3333}).then(() => {
   console.log('Ain ze da manga')
